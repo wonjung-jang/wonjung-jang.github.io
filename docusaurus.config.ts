5eb280d295ff499260de7d63df74906894be00d2
wonjung-jang <jwj@dwhale.kr>
2025-07-26 12:55:05 +0900

    feat: social card 이미지 생성 및 설정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,8 +51,8 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    // 프로젝트의 소셜 카드로 교체하세요
+    image: "img/social-card.jpg",
     colorMode: {
       defaultMode: "light",
       disableSwitch: true,
