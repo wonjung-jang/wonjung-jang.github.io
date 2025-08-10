833198fd02cb73735a9f9689626d02e89943deee
wonjung-jang <jwj@dwhale.kr>
2025-08-11 00:13:22 +0900

    feat: ImageWrapper, ImageSlider에 gap 추가

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -6,14 +6,18 @@ interface ImageSliderProps {
     alt: string;
     height: string;
   }[];
+  description?: string;
 }
 
-const ImageSlider = ({ images }: ImageSliderProps) => {
+const ImageSlider = ({ images, description }: ImageSliderProps) => {
   return (
-    <div className={styles.slider}>
-      {images.map((image) => (
-        <img src={image.src} alt={image.alt} height={image.height} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.slider}>
+        {images.map((image) => (
+          <img src={image.src} alt={image.alt} height={image.height} />
+        ))}
+      </div>
+      {description && <p className={styles.description}>{description}</p>}
     </div>
   );
 };
